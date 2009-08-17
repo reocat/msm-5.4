904f438791af465e153e9f1bbe824382abccb304
rrcao <rrcao@ambarella.com>
2009-08-17 21:53:46 +0800

    Add Patch Version Info and Update CS42L51 Codec Driver

---
+++ arch/arm/mach-ambarella/include/mach/patch-ver.h
@@ -0,0 +1,16 @@
+/*
+ * include/asm-arm/arch-ambarella/patch-ver.h
+ *
+ * History:
+ *    2009/08/17 - [Cao Rongrong] created file
+ *
+ * Copyright (C) 2006-2007, Ambarella, Inc.
+ */
+
+#ifndef __ASM_ARCH_PATCH_VER_H
+#define __ASM_ARCH_PATCH_VER_H
+
+#define	Ambarella_Patch_Version		"0001(2009-08-17)"
+
+#endif
+
