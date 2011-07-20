83b333765063a85e1312d32d8e030b69980ea5ff
Bing-Liang Hu <blhu@ambarella.com>
2011-07-20 10:26:40 +0800

    add wm831x gpio poweroff
    Change-Id: If5f454cabe01fe54e2f827954319856887170859

--- drivers/mfd/wm831x-spi.c
+++ drivers/mfd/wm831x-spi.c
@@ -17,6 +17,7 @@
 #include <linux/spi/spi.h>
 #include <linux/mfd/wm831x/irq.h>
 #include <linux/mfd/wm831x/core.h>
+#include <linux/mfd/wm831x/gpio.h>
 
 #include <linux/swab.h>
 static int wm831x_spi_read_device(struct wm831x *wm831x, unsigned short reg,
@@ -117,7 +118,14 @@ static int __devexit wm831x_spi_remove(struct spi_device *spi)
 
 static int wm831x_spi_suspend(struct spi_device *spi, pm_message_t m)
 {
+	int ret   = 0;
 	struct wm831x *wm831x = dev_get_drvdata(&spi->dev);
+
+	wm831x_reg_write(wm831x, WM831X_GPIO5_CONTROL,
+					WM831X_GPN_DIR| WM831X_GPN_ENA | WM831X_SLEEP_REQUEST | WM831X_GPN_POL | WM831X_GPIO_PULL_NONE);
+	if (ret != 0)
+		dev_err(wm831x->dev, "Fail to set sleep request with error: %d\n",
+			ret);
 
 	return wm831x_device_suspend(wm831x);
 }
