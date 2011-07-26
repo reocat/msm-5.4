d240a037cc275680cc715326a2b9b7537541616c
Iiro Valkonen <iiro.valkonen@atmel.com>
2011-07-26 17:30:02 +0000

    Input: atmel_mxt_ts - Make wait-after-reset period compatible with all chips
    The delay before the chip can be accessed after reset varies between different
    chips in maXTouch family. Waiting for 200ms and then monitoring the CHG (chip
    is ready when the line is low) is guaranteed to work with all chips.
    
    v5: Fix error in timeout_counter handling
        Implement separate reset time for mXT768E
        Increase sleep time for backup check. msleep for small values is unreliable
    v4: Adjust delay depending on the family ID of the chip. Also add a readback
        of command register after backup is issued, to make sure we are not proceeding
        too fast there.
    v3: Add a check for NULL read_chg() function, and add the read_chg() to platform
        files using this driver (currently only mach-goni.c)
    v2: At Dmitry's suggestion, add a timeout so we are not stuck looping
        endlessly in case the CHG is not going low.
    
    Signed-off-by: Iiro Valkonen <iiro.valkonen@atmel.com>
    Acked-by: Nick Dyer <nick.dyer@itdev.co.uk>

--- drivers/input/touchscreen/atmel_mxt_ts.c
+++ drivers/input/touchscreen/atmel_mxt_ts.c
@@ -2,6 +2,7 @@
  * Atmel maXTouch Touchscreen driver
  *
  * Copyright (C) 2010 Samsung Electronics Co.Ltd
+ * Copyright (C) 2011 Atmel Corporation
  * Author: Joonyoung Shim <jy0922.shim@samsung.com>
  *
  * This program is free software; you can redistribute  it and/or modify it
@@ -20,6 +21,11 @@
 #include <linux/input.h>
 #include <linux/interrupt.h>
 #include <linux/slab.h>
+
+/* Family ID */
+#define MXT224_ID		0x80
+#define MXT768E_ID		0xA1
+#define MXT1386_ID		0xA0
 
 /* Version */
 #define MXT_VER_20		20
@@ -175,8 +181,14 @@
 /* Define for MXT_GEN_COMMAND_T6 */
 #define MXT_BOOT_VALUE		0xa5
 #define MXT_BACKUP_VALUE	0x55
+
+/* Delay times */
 #define MXT_BACKUP_TIME		25	/* msec */
-#define MXT_RESET_TIME		65	/* msec */
+#define MXT224_RESET_TIME	65	/* msec */
+#define MXT768E_RESET_TIME	250	/* msec */
+#define MXT1386_RESET_TIME	200	/* msec */
+#define MXT_RESET_TIME		200	/* msec */
+#define MXT_RESET_NOCHGREAD	400	/* msec */
 
 #define MXT_FWRESET_TIME	175	/* msec */
 
@@ -820,7 +832,9 @@ static int mxt_initialize(struct mxt_data *data)
 	struct i2c_client *client = data->client;
 	struct mxt_info *info = &data->info;
 	int error;
+	int timeout_counter = 0;
 	u8 val;
+	u8 command_register;
 
 	error = mxt_get_info(data);
 	if (error)
@@ -851,11 +865,46 @@ static int mxt_initialize(struct mxt_data *data)
 			MXT_COMMAND_BACKUPNV,
 			MXT_BACKUP_VALUE);
 	msleep(MXT_BACKUP_TIME);
+	do {
+		error =  mxt_read_object(data, MXT_GEN_COMMAND_T6,
+					 MXT_COMMAND_BACKUPNV,
+					 &command_register);
+		if (error)
+			return error;
+		msleep(10);
+	} while ((command_register != 0) && (timeout_counter++ <= 100));
+	if (timeout_counter > 100) {
+		dev_err(&client->dev, "No response after backup!\n");
+		return -EIO;
+	}
 
 	/* Soft reset */
 	mxt_write_object(data, MXT_GEN_COMMAND_T6,
 			MXT_COMMAND_RESET, 1);
-	msleep(MXT_RESET_TIME);
+	if (data->pdata->read_chg == NULL) {
+		msleep(MXT_RESET_NOCHGREAD);
+	} else {
+		switch (info->family_id) {
+		case MXT224_ID:
+			msleep(MXT224_RESET_TIME);
+			break;
+		case MXT768E_ID:
+			msleep(MXT768E_RESET_TIME);
+			break;
+		case MXT1386_ID:
+			msleep(MXT1386_RESET_TIME);
+			break;
+		default:
+			msleep(MXT_RESET_TIME);
+		}
+		timeout_counter = 0;
+		while ((timeout_counter++ <= 100) && data->pdata->read_chg())
+			msleep(10);
+		if (timeout_counter > 100) {
+			dev_err(&client->dev, "No response after reset!\n");
+			return -EIO;
+		}
+	}
 
 	/* Update matrix size at info struct */
 	error = mxt_read_reg(client, MXT_MATRIX_X_SIZE, &val);
