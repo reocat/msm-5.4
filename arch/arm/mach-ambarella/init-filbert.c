5c474f47b3c84a3711753b239d015129363f0c3b
Mike Xue <zwxue@ambarella.com>
2011-08-19 11:47:51 +0800

    Add SPI2 into A7
    Change-Id: If3c49c07d7b46413eba795c1d954383a4f411eee

--- arch/arm/mach-ambarella/init-filbert.c
+++ arch/arm/mach-ambarella/init-filbert.c
@@ -78,7 +78,7 @@ static struct platform_device *ambarella_devices[] __initdata = {
 	&ambarella_sd0,
 	&ambarella_spi0,
 	&ambarella_spi1,
-//	&ambarella_spi2,
+	&ambarella_spi2,
 	&ambarella_uart,
 	&ambarella_uart1,
 	&ambarella_udc0,
