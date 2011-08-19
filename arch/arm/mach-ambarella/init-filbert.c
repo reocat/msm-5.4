37a20922eec4d32281b4e3f950a2677394b48365
Mike Xue <zwxue@ambarella.com>
2011-08-19 09:19:05 +0800

    Update A7 SPI
    Change-Id: I6036c15a41725b5d21f186e9b598e0d9df6961c8

--- arch/arm/mach-ambarella/init-filbert.c
+++ arch/arm/mach-ambarella/init-filbert.c
@@ -99,8 +99,68 @@ static struct spi_board_info ambarella_spi_devices[] = {
 	},
 	{
 		.modalias	= "spidev",
+		.bus_num	= 0,
+		.chip_select	= 2,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 0,
+		.chip_select	= 3,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 0,
+		.chip_select	= 4,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 0,
+		.chip_select	= 5,
+	},
+	{
+		.modalias	= "spidev",
 		.bus_num	= 1,
 		.chip_select	= 0,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 2,
+		.chip_select	= 0,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 2,
+		.chip_select	= 1,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 2,
+		.chip_select	= 2,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 2,
+		.chip_select	= 3,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 2,
+		.chip_select	= 4,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 2,
+		.chip_select	= 5,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 2,
+		.chip_select	= 6,
+	},
+	{
+		.modalias	= "spidev",
+		.bus_num	= 2,
+		.chip_select	= 7,
 	},
 };
 
