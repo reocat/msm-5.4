9192327a85633e298f5ec9b63732f055e2432924
Nick Dyer <nick.dyer@itdev.co.uk>
2011-11-15 13:52:56 +0000

    Input: atmel_mxt_ts - Improve device configuration
    Implement new raw configuration format using firmware loader
    Remove unnecessary platform data configuration
    Rework power init behaviour
    Rework resolution calculation behaviour
    
    Signed-off-by: Nick Dyer <nick.dyer@itdev.co.uk>

--- include/linux/i2c/atmel_mxt_ts.h
+++ include/linux/i2c/atmel_mxt_ts.h
@@ -27,20 +27,8 @@
 
 /* The platform data for the Atmel maXTouch touchscreen driver */
 struct mxt_platform_data {
-	const u8 *config;
-	size_t config_length;
-
-	unsigned int x_line;
-	unsigned int y_line;
-	unsigned int x_size;
-	unsigned int y_size;
-	unsigned int blen;
-	unsigned int threshold;
-	unsigned int voltage;
-	unsigned char orient;
 	unsigned long irqflags;
 	u8(*read_chg) (void);
-	unsigned long config_crc;
 };
 
 #endif /* __LINUX_ATMEL_MXT_TS_H */
