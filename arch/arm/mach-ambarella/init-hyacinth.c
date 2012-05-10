39855d306867b7ea81546e67ddbaa2b8e9e79aa7
Tzu-Jung Lee <tjlee@ambarella.com>
2012-05-10 11:58:13 +0800

    a8: add rct.[ch], init-hyacinth.c

---
+++ arch/arm/mach-ambarella/init-hyacinth.c
@@ -0,0 +1,83 @@
+/*
+ * arch/arm/mach-ambarella/init-generic.c
+ *
+ * Author: Anthony Ginger <hfjiang@ambarella.com>
+ *
+ * Copyright (C) 2004-2010, Ambarella, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/dma-mapping.h>
+#include <linux/i2c.h>
+
+#include <asm/mach-types.h>
+#include <asm/mach/arch.h>
+
+#include <mach/hardware.h>
+#include <mach/init.h>
+#include <mach/board.h>
+
+#include <linux/irq.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
+
+#include <plat/ambinput.h>
+
+#include "board-device.h"
+
+/* ==========================================================================*/
+static struct platform_device *ambarella_devices[] __initdata = {
+#ifdef CONFIG_PLAT_AMBARELLA_SUPPORT_HW_CRYPTO
+	&ambarella_crypto,
+#endif
+	&ambarella_dummy_codec0,
+	&ambarella_eth0,
+	&ambarella_rtc0,
+	&ambarella_uart,
+	&ambarella_uart1,
+};
+
+/* ==========================================================================*/
+static void __init ambarella_init_generic(void)
+{
+	int					i;
+
+	ambarella_init_machine("Hyacinth");
+
+	platform_add_devices(ambarella_devices, ARRAY_SIZE(ambarella_devices));
+	for (i = 0; i < ARRAY_SIZE(ambarella_devices); i++) {
+		device_set_wakeup_capable(&ambarella_devices[i]->dev, 1);
+		device_set_wakeup_enable(&ambarella_devices[i]->dev, 0);
+	}
+
+	ambarella_eth0_platform_info.mii_id = 0;
+	ambarella_eth0_platform_info.phy_id = 0x001cc915;
+}
+
+/* ==========================================================================*/
+MACHINE_START(HYACINTH, "Hyacinth")
+	.boot_params	= CONFIG_AMBARELLA_PARAMS_PHYS,
+	.map_io		= ambarella_map_io,
+	.reserve	= ambarella_memblock_reserve,
+	.init_irq	= ambarella_init_irq,
+	.timer		= &ambarella_timer,
+	.init_machine	= ambarella_init_generic,
+MACHINE_END
+
