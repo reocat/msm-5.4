24be05fd6866cfc3fa27c4ee2fefa6bf90272701
Rong-Rong Cao <rrcao@ambarella.com>
2013-05-31 16:00:30 +0800

    Update S2 HAL to 7186
    Change-Id: Ifd9749fb4226b8621ff122fd53c0544eda131b3b

--- arch/arm/plat-ambarella/include/hal/s2/ambhal.h
+++ arch/arm/plat-ambarella/include/hal/s2/ambhal.h
@@ -4,7 +4,7 @@
  * @author Mahendra Lodha <mlodha@ambarella.com>
  * @author Rudi Rughoonundon <rudir@ambarella.com>
  * @date January 2012
- * @version 7185
+ * @version 7186
  *
  * @par Introduction:
  * The Ambarella A7S Hardware Abstraction Layer (ambhal) provides an API between
