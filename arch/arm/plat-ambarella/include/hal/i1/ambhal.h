24c42e6eb00d93a929b2c008b40cd45156478a6f
Rong-Rong Cao <rrcao@ambarella.com>
2013-06-08 14:51:47 +0800

    Update iONE HAL to 206575
    Change-Id: Ie4f4c88e67614c2139a9a779ba9876d2051063f9

--- arch/arm/plat-ambarella/include/hal/i1/ambhal.h
+++ arch/arm/plat-ambarella/include/hal/i1/ambhal.h
@@ -4,7 +4,7 @@
  * @author Mahendra Lodha <mlodha@ambarella.com>
  * @author Rudi Rughoonundon <rudir@ambarella.com>
  * @date June 2010
- * @version 172770
+ * @version 206575
  *
  * @par Introduction:
  * The Ambarella I1 Hardware Abstraction Layer (AMBHAL) provides an API between
