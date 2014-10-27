3699ea8aa5e673a424e973f2b64cb48b4fcffd89
ccyao <ccyao@ambarella.com>
2014-10-27 17:15:53 +0800

    Networking: add AMBA MAC support
    *apply wifi and bt MAC from kernel cmdline
      / # cat /proc/ambarella/board_info
      wifi_mac: 00:03:7F:14:24:33
      wifi1_mac: 00:03:7F:14:24:44
    
    *example cmdline:
    char KernelCmdLine[] = "ubi.mtd=8 root=ubi0:linux rootfstype=ubifs console=ttyS1 nr_cpus=1 maxcpus=0 wifi_mac=00:03:7F:14:24:33 bt_mac=00:03:7F:14:24:44";
    
    Change-Id: If47958cd3cd655b42b02da93ab86dcd069c09e93

--- arch/arm/mach-ambarella/ambalink/init.c
+++ arch/arm/mach-ambarella/ambalink/init.c
@@ -479,6 +479,60 @@ static int __init early_boss(char *p)
        return 0;
 }
 early_param("boss", early_boss);
+
+char wifi_mac[20];
+EXPORT_SYMBOL(wifi_mac);
+static int __init early_wifi_mac(char *p)
+{
+	strncpy(wifi_mac, p, strlen("00:00:00:00:00:00"));
+	wifi_mac[strlen("00:00:00:00:00:00")] = '\0';
+
+	return 0;
+}
+early_param("wifi_mac", early_wifi_mac);
+
+char bt_mac[20];
+EXPORT_SYMBOL(bt_mac);
+static int __init early_bt_mac(char *p)
+{
+	strncpy(bt_mac, p, strlen("00:00:00:00:00:00"));
+	bt_mac[strlen("00:00:00:00:00:00")] = '\0';
+
+	return 0;
+}
+early_param("bt_mac", early_bt_mac);
+
+static int ambarella_atags_proc_read(struct seq_file *m, void *v)
+{
+	int retlen = 0;
+
+	/* keep the the same name as old platform */
+	retlen += seq_printf(m, "wifi_mac: %s\n", wifi_mac);
+	retlen += seq_printf(m, "wifi1_mac: %s\n", bt_mac);
+	return retlen;
+}
+
+static int ambarella_atags_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, ambarella_atags_proc_read, NULL);
+}
+
+static const struct file_operations atags_ambarella_proc_fops = {
+	.open = ambarella_atags_proc_open,
+	.read = seq_read,
+	.llseek	= seq_lseek,
+	.release = single_release,
+};
+
+static int __init atags_ambarella_proc_init(void)
+{
+	/* keep the name board_info to old platform */
+	proc_create("board_info", S_IRUGO, get_ambarella_proc_dir(),
+		&atags_ambarella_proc_fops);
+
+	return 0;
+}
+late_initcall(atags_ambarella_proc_init);
 #endif
 
 u32 ambarella_phys_to_virt(u32 paddr)
