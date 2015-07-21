452c2515b07ca89330d8bc0f00c4cdc5b092f2f3
Xian-Qing Zheng <xqzheng@ambarella.com>
2015-07-21 10:30:41 +0800

    repair the bug 1000M speed will stop when run dewarp
    Change-Id: Ibe385b1e1708b4839b55595eb2d8bf67ad1b0377

--- drivers/net/ethernet/ambarella/ambarella_eth_normal.c
+++ drivers/net/ethernet/ambarella/ambarella_eth_normal.c
@@ -405,7 +405,8 @@ static inline int ambhw_enable(struct ambeth_info *lp)
 	amba_writel(lp->regbase + ETH_DMA_OPMODE_OFFSET,
 				ETH_DMA_OPMODE_TTC_256 |
 				ETH_DMA_OPMODE_RTC_64 |
-				ETH_DMA_OPMODE_FUF);
+				ETH_DMA_OPMODE_FUF |
+				ETH_DMA_OPMODE_TSF);
 	amba_writel(lp->regbase + ETH_MAC_CFG_OFFSET,
 		(ETH_MAC_CFG_TE | ETH_MAC_CFG_RE));
 
