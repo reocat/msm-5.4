a44b105f9c41871a29b2b8f190cca1419a55f67b
Jon <tdlin@ambarella.com>
2015-10-27 19:01:33 +0800

    Fix modprobe -r SENSOR panic
    Change-Id: Iaa7d9e4081090acd1c36e29ba92b3b33feddae31

--- drivers/spi/spi-ambarella.c
+++ drivers/spi/spi-ambarella.c
@@ -178,6 +178,9 @@ static void ambarella_spi_setup(struct ambarella_spi *bus, struct spi_device *sp
 
 static void ambarella_spi_stop(struct ambarella_spi *bus)
 {
+    if (!bus)
+        return;
+
 	gpio_set_value(bus->msg->spi->cs_gpio, 1);
 	bus->cs_active = 0;
 
