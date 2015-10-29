03244d60489ccec8a39cfc7e6a497b2bf432fdd1
Jon <tdlin@ambarella.com>
2015-10-29 10:40:21 +0800

    Typo fixed
    Change-Id: Ia39a79e51a9784f1a7430616a0b7f6cf02195c08

--- drivers/spi/spi-ambarella.c
+++ drivers/spi/spi-ambarella.c
@@ -178,7 +178,7 @@ static void ambarella_spi_setup(struct ambarella_spi *bus, struct spi_device *sp
 
 static void ambarella_spi_stop(struct ambarella_spi *bus)
 {
-    if (!bus || !bus->msg || !bus->msg_spi)
+    if (!bus || !bus->msg || !bus->msg->spi)
         return;
    
 	gpio_set_value(bus->msg->spi->cs_gpio, 1);
