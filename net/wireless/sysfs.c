b37c668966785850eac8d20b921b36b436ce65ec
ccyao <ccyao@ambarella.com>
2016-04-14 14:21:13 +0800

    WoWLAN: do not disconnect all STA in AP mode suspend
    Change-Id: Iedcf69639254a992befaede591a649455620f614

--- net/wireless/sysfs.c
+++ net/wireless/sysfs.c
@@ -104,8 +104,10 @@ static int wiphy_suspend(struct device *dev)
 
 	rtnl_lock();
 	if (rdev->wiphy.registered) {
+#ifndef CONFIG_ARCH_AMBARELLA_AMBALINK
 		if (!rdev->wiphy.wowlan_config)
 			cfg80211_leave_all(rdev);
+#endif
 		if (rdev->ops->suspend)
 			ret = rdev_suspend(rdev, rdev->wiphy.wowlan_config);
 		if (ret == 1) {
