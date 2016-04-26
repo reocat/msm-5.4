42300b578d2f56e26f0068303523f2b4d942bd05
Sam Ho (chho) <chho@ambarella.com>
2016-04-26 10:27:50 +0800

    Networking: add AMBA MAC support
    * Apply WiFi and BT MAC from kernel command line:
        / # cat /proc/cmdline
        root=/dev/mtdblock5 rootfstype=squashfs console=ttyS1 nr_cpus=4 maxcpus=4 swiotlb=1024 wifi_mac=00:03:7F:14:24:33 bt_mac=00:03:7F:14:24:44
        / # cat /proc/ambarella/board_info
        wifi_mac: 00:03:7F:14:24:33
        wifi1_mac: 00:03:7F:14:24:44
    
    * Example cmdline:
        (at .config)
        CONFIG_AMBALINK_CMDLINE="root=/dev/mtdblock5 rootfstype=squashfs console=ttyS1 nr_cpus=4 maxcpus=4 swiotlb=1024 wifi_mac=00:03:7F:14:24:33 bt_mac=00:03:7F:14:24:44"
        or
        (at bsp/h2evk_everest/AmbaLinkMemoryCfg.c)
        char KernelCmdLine[] = "ubi.mtd=8 root=ubi0:linux rootfstype=ubifs console=ttyS1 nr_cpus=1 maxcpus=0 wifi_mac=00:03:7F:14:24:33 bt_mac=00:03:7F:14:24:44";
    
    Change-Id: Ifb03be8dfd2237b6cd81adc5eb66f9805011c741

--- drivers/soc/ambarella/init_misc.c
+++ drivers/soc/ambarella/init_misc.c
@@ -23,6 +23,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 
 #ifdef CONFIG_ARCH_AMBARELLA_AMBALINK
 #include <plat/ambalink_cfg.h>
@@ -61,5 +62,61 @@ static int __init ambarella_init_misc(void)
 core_initcall(ambarella_init_misc);
 #else
 core_initcall(ambarella_init_root_proc);
+#endif
+
+#ifdef CONFIG_ARCH_AMBARELLA_AMBALINK
+char wifi_mac[20];
+EXPORT_SYMBOL(wifi_mac);
+static int __init early_wifi_mac(char *p)
+{
+	strncpy(wifi_mac, p, strlen("00:00:00:00:00:00"));
+	wifi_mac[strlen("00:00:00:00:00:00")] = '\0';
+
+	return 0;
+}
+early_param("wifi_mac", early_wifi_mac);
+
+char bt_mac[20];
+EXPORT_SYMBOL(bt_mac);
+static int __init early_bt_mac(char *p)
+{
+	strncpy(bt_mac, p, strlen("00:00:00:00:00:00"));
+	bt_mac[strlen("00:00:00:00:00:00")] = '\0';
+
+	return 0;
+}
+early_param("bt_mac", early_bt_mac);
+
+static int ambarella_atags_proc_read(struct seq_file *m, void *v)
+{
+
+	/* keep the the same name as old platform */
+	seq_printf(m, "wifi_mac: %s\n", wifi_mac);
+	seq_printf(m, "wifi1_mac: %s\n", bt_mac);
+	return 0;
+}
+
+static int ambarella_atags_proc_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, ambarella_atags_proc_read, NULL);
+}
+
+static const struct file_operations atags_ambarella_proc_fops = {
+	.open = ambarella_atags_proc_open,
+	.read = seq_read,
+	.llseek	= seq_lseek,
+	.release = single_release,
+};
+
+static int __init atags_ambarella_proc_init(void)
+{
+	/* keep the name board_info to old platform */
+	proc_create("board_info", S_IRUGO, get_ambarella_proc_dir(),
+		&atags_ambarella_proc_fops);
+
+	return 0;
+}
+late_initcall(atags_ambarella_proc_init);
+
 #endif
 
