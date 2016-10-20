raspberrypi/linux

cpufreq: Temporarily ignore io_is_busy=1
To speed testing of the new sdhost driver that adapts to changes in
core_freq, hack the on-demand governor to treat io_is_busy=1 as
io_is_busy=0. The io_is_busy feature can still be forced using
io_is_busy=2.

Signed-off-by: Phil Elwell <phil@raspberrypi.org>

drivers/cpufreq/cpufreq_ondemand.c
@@ -307,7 +307,12 @@ static ssize_t store_io_is_busy(struct dbs_data *dbs_data, const char *buf,
 	ret = sscanf(buf, "%u", &input);
 	if (ret != 1)
 		return -EINVAL;
-	od_tuners->io_is_busy = !!input;
+	// XXX temporary hack
+	if (input > 1)
+		input = 1;
+	else
+		input = 0;
+	od_tuners->io_is_busy = input;
 
 	/* we need to re-evaluate prev_cpu_idle */
 	for_each_online_cpu(j) {

reocat/msm-5.4

drivers/cpufreq/cpufreq_ondemand.c
@@ -212,7 +212,12 @@ static ssize_t store_io_is_busy(struct gov_attr_set *attr_set, const char *buf,
 	ret = sscanf(buf, "%u", &input);
 	if (ret != 1)
 		return -EINVAL;
-	dbs_data->io_is_busy = !!input;
+	// XXX temporary hack
+	if (input > 1)
+		input = 1;
+	else
+		input = 0;
+	dbs_data->io_is_busy = input;
 
 	/* we need to re-evaluate prev_cpu_idle */
 	gov_update_cpu_data(dbs_data);