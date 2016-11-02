c6dcbf6f4c6d969960133db2e7ead253e1544390
Jon <tdlin@ambarella.com>
2016-11-02 15:33:24 +0800

    Fix header comments
    Change-Id: Idaf2cfcd99a36a89ae4175185ad78f8ab395b385

--- drivers/net/phy/supercam.c
+++ drivers/net/phy/supercam.c
@@ -1,16 +1,13 @@
 /*
- * Driver for AMD am79c PHYs
+ * Driver for Supercam PHY
  *
- * Author: Heiko Schocher <hs@denx.de>
+ * Author: Jon <tdlin@ambarella.com>
  *
- * Copyright (c) 2011 DENX Software Engineering GmbH
+ * Copyright (c) 2016 Ambarella
  *
- * This program is free software; you can redistribute  it and/or modify it
- * under  the terms of  the GNU General  Public License as published by the
- * Free Software Foundation;  either version 2 of the  License, or (at your
- * option) any later version.
  *
  */
+
 #include <linux/kernel.h>
 #include <linux/errno.h>
 #include <linux/init.h>
