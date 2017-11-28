91de35ea8350f55ed65bd0de2cabe9aa86770bf0
Cao Rongrong <rrcao@ambarella.com>
2017-11-28 12:10:24 +0800

    Trivial Update
    Change-Id: I7c39b9e498e7abb72815d04ce5d77d6f2c1acfd9

--- drivers/usb/host/ohci-ambarella.c
+++ drivers/usb/host/ohci-ambarella.c
@@ -193,7 +193,6 @@ amb_ohci_err:
 static int ohci_hcd_ambarella_drv_remove(struct platform_device *pdev)
 {
 	struct usb_hcd *hcd = platform_get_drvdata(pdev);
-	struct ohci_ambarella *amb_ohci = hcd_to_ohci_ambarella(hcd);
 
 	usb_remove_hcd(hcd);
 	usb_put_hcd(hcd);
