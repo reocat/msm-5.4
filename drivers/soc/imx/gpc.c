fe5c180101394d25f1966ebe7a8363c483dd9db0
Leonard Crestez <leonard.crestez@nxp.com>
2018-02-22 17:23:26 +0200

    (TS-IMX-REBASE) soc: imx: gpc: Restore _imx6_pm_domain_power_on
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    
    Casey: Conflict with https://github.com/Whissi/linux-stable/commit/617dcdd3d27d0f5059bafe88d44750b13efc4e5c
    Just keep 'ret' integer for added timeout block.

--- drivers/soc/imx/gpc.c
+++ drivers/soc/imx/gpc.c
@@ -108,20 +108,11 @@ static int imx6_pm_domain_power_off(struct generic_pm_domain *genpd)
 	return 0;
 }
 
-static int imx6_pm_domain_power_on(struct generic_pm_domain *genpd)
+static void _imx6_pm_domain_power_on(struct generic_pm_domain *genpd)
 {
 	struct imx_pm_domain *pd = to_imx_pm_domain(genpd);
 	int i, ret;
 	u32 val, req;
-
-	if (pd->supply) {
-		ret = regulator_enable(pd->supply);
-		if (ret) {
-			pr_err("%s: failed to enable regulator: %d\n",
-			       __func__, ret);
-			return ret;
-		}
-	}
 
 	/* Enable reset clocks for all devices in the domain */
 	for (i = 0; i < pd->num_clks; i++)
@@ -147,6 +138,23 @@ static int imx6_pm_domain_power_on(struct generic_pm_domain *genpd)
 	/* Disable reset clocks for all devices in the domain */
 	for (i = 0; i < pd->num_clks; i++)
 		clk_disable_unprepare(pd->clk[i]);
+}
+
+static int imx6_pm_domain_power_on(struct generic_pm_domain *genpd)
+{
+	struct imx_pm_domain *pd = to_imx_pm_domain(genpd);
+	int ret;
+
+	if (pd->supply) {
+		ret = regulator_enable(pd->supply);
+		if (ret) {
+			pr_err("%s: failed to enable regulator: %d\n",
+			       __func__, ret);
+			return ret;
+		}
+	}
+
+	_imx6_pm_domain_power_on(genpd);
 
 	return 0;
 }
