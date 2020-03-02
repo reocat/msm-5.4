bce71c987dea02f1c40f1d65bdf4356c1297a2c3
Anson Huang <Anson.Huang@nxp.com>
2020-03-02 16:54:26 +0800

    thermal: imx_thermal: Use __maybe_unused instead of CONFIG_PM_SLEEP
    Use __maybe_unused for power management related functions instead
    of #if CONFIG_PM_SLEEP to simplify the code.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Link: https://lore.kernel.org/r/1583139266-23615-1-git-send-email-Anson.Huang@nxp.com
    (cherry picked from commit b009514f9786e50578c52eeef6d2020f84630773)
    Signed-off-by: Shrirang Bagul <shrirang.bagul@canonical.com>

--- drivers/thermal/imx_thermal.c
+++ drivers/thermal/imx_thermal.c
@@ -879,8 +879,7 @@ static int imx_thermal_remove(struct platform_device *pdev)
 	return 0;
 }
 
-#ifdef CONFIG_PM_SLEEP
-static int imx_thermal_suspend(struct device *dev)
+static int __maybe_unused imx_thermal_suspend(struct device *dev)
 {
 	struct imx_thermal_data *data = dev_get_drvdata(dev);
 	struct regmap *map = data->tempmon;
@@ -901,7 +900,7 @@ static int imx_thermal_suspend(struct device *dev)
 	return 0;
 }
 
-static int imx_thermal_resume(struct device *dev)
+static int __maybe_unused imx_thermal_resume(struct device *dev)
 {
 	struct imx_thermal_data *data = dev_get_drvdata(dev);
 	struct regmap *map = data->tempmon;
@@ -919,7 +918,6 @@ static int imx_thermal_resume(struct device *dev)
 
 	return 0;
 }
-#endif
 
 static SIMPLE_DEV_PM_OPS(imx_thermal_pm_ops,
 			 imx_thermal_suspend, imx_thermal_resume);
