e73fa548daa6a395e54ffc29dd8b6f6cf6789719
Thinh Nguyen <Thinh.Nguyen@synopsys.com>
2020-03-05 13:24:14 -0800

    usb: dwc3: gadget: Remove unnecessary checks
    Remove 2 unnecessary checks:
    1) A request in the started_list must have its trb field set. So
    checking for req->trb is unnecessary.
    
    2) An endpoint must have started (and have not ended) for the request to
    still be in the started_list. There's no point to check if the endpoint
    is started. We had this check because previously the driver didn't
    handle the endpoint's started/ended flags for END_TRANSFER command
    properly. See commit 9f45581f5eec ("usb: dwc3: gadget: early giveback
    if End Transfer already completed").
    
    Signed-off-by: Thinh Nguyen <thinhn@synopsys.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>
    (cherry picked from commit 8411993e79df8e54da67613025d620a8a23a24fe)
    Signed-off-by: Shrirang Bagul <shrirang.bagul@canonical.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -1670,9 +1670,6 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 			/* wait until it is processed */
 			dwc3_stop_active_transfer(dep, true, true);
 
-			if (!r->trb)
-				goto out0;
-
 			/*
 			 * Remove any started request if the transfer is
 			 * cancelled.
@@ -1680,10 +1677,7 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 			list_for_each_entry_safe(r, t, &dep->started_list, list)
 				dwc3_gadget_move_cancelled_request(r);
 
-			if (dep->flags & DWC3_EP_TRANSFER_STARTED)
-				goto out0;
-			else
-				goto out1;
+			goto out0;
 		}
 		dev_err(dwc->dev, "request %pK was not queued to %s\n",
 				request, ep->name);
@@ -1691,7 +1685,6 @@ static int dwc3_gadget_ep_dequeue(struct usb_ep *ep,
 		goto out0;
 	}
 
-out1:
 	dwc3_gadget_giveback(dep, req, -ECONNRESET);
 
 out0:
