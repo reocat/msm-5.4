2aeb2f639f71a3d91c581dc28e5876d834aa1021
Rishabh Bhatnagar <rishabhb@codeaurora.org>
2020-07-16 15:20:33 -0700

    remoteproc: Pass size and offset as arguments to segment dump function
    Change the segment dump API signature to include size and offset
    arguments. Refactor the qcom_q6v5_mss driver to use these
    arguments while copying the segment. Doing this lays the ground
    work for "inline" coredump functionality being added in the next
    patch.
    
    Tested-by: Sibi Sankar <sibis@codeaurora.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Reviewed-by: Sibi Sankar <sibis@codeaurora.org>
    Reviewed-by: Mathieu Poirier <mathieu.poirier@linaro.org>
    Signed-off-by: Rishabh Bhatnagar <rishabhb@codeaurora.org>
    Link: https://lore.kernel.org/r/1594938035-7327-4-git-send-email-rishabhb@codeaurora.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    (backported from commit 76abf9cea6c8215ea45b9359f11f0e30127c544a)
    Signed-off-by: Shrirang Bagul <shrirang.bagul@canonical.com>

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -458,7 +458,7 @@ struct rproc_dump_segment {
 
 	void *priv;
 	void (*dump)(struct rproc *rproc, struct rproc_dump_segment *segment,
-		     void *dest);
+		     void *dest, size_t offset, size_t size);
 	loff_t offset;
 };
 
@@ -640,7 +640,8 @@ int rproc_coredump_add_custom_segment(struct rproc *rproc,
 				      dma_addr_t da, size_t size,
 				      void (*dumpfn)(struct rproc *rproc,
 						     struct rproc_dump_segment *segment,
-						     void *dest),
+						     void *dest, size_t offset,
+						     size_t size),
 				      void *priv);
 int rproc_coredump_set_elf_info(struct rproc *rproc, u8 class, u16 machine);
 
