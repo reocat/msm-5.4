microsoft/surface-duo-oss-kernel.msm-4..14

ath11k: setup QCA6390 rings for both rxdmas
For QCA6390, only one pdev is created and this pdev manages both
lmacs, thus both rxdmas. So host needs to initialize all rxdma
related rings for one pdev.

Another difference is for QCA6390, host fills rxbuf to fw and fw
further fills the rxbuf to rxbuf ring for each rxdma.

Tested QCA6390 on X86 platform.
Tested firmware WLAN.HST.1.0.1.c1-00440-QCAHSTSWPLZ_V2_TO_X86-1.

Signed-off-by : Carl Huang <cjhuang@codeaurora.org>

Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

drivers/net/wireless/ath/ath11k/hw.c
@@ -205,10 +205,34 @@ static void ath11k_init_wmi_config_ipq8074(struct ath11k_base *ab,
 	config->twt_ap_sta_count = 1000;
 }
 
+static int ath11k_mac_id_to_pdev_id_ipq8074(int mac_id)
+{
+	return mac_id;
+}
+
+static int ath11k_mac_id_to_srng_id_ipq8074(int mac_id)
+{
+	return 0;
+}
+
+static int ath11k_mac_id_to_pdev_id_qca6x90(int mac_id)
+{
+	return 0;
+}
+
+static int ath11k_mac_id_to_srng_id_qca6x90(int mac_id)
+{
+	return mac_id;
+}
+
 const struct ath11k_hw_ops ath11k_hw_ops_qca6x90 = {
 	.wmi_init_config = ath11k_init_wmi_config_qca6x90,
+	.mac_id_to_pdev_id = ath11k_mac_id_to_pdev_id_qca6x90,
+	.mac_id_to_srng_id = ath11k_mac_id_to_srng_id_qca6x90,
 };
 
 const struct ath11k_hw_ops ath11k_hw_ops_ipq8074 = {
 	.wmi_init_config = ath11k_init_wmi_config_ipq8074,
+	.mac_id_to_pdev_id = ath11k_mac_id_to_pdev_id_ipq8074,
+	.mac_id_to_srng_id = ath11k_mac_id_to_srng_id_ipq8074,
 };

reocat/msm-5.4

drivers/net/wireless/ath/ath11k/hw.c
@@ -182,18 +182,45 @@ static void ath11k_init_wmi_config_ipq8074(struct ath11k_base *ab,
 	config->twt_ap_sta_count = 1000;
 }
 
+static int ath11k_mac_id_to_pdev_id_ipq8074(int mac_id)
+{
+	return mac_id;
+}
+
+static int ath11k_mac_id_to_srng_id_ipq8074(int mac_id)
+{
+	return 0;
+}
+
+static int ath11k_mac_id_to_pdev_id_qca6390(int mac_id)
+{
+	return 0;
+}
+
+static int ath11k_mac_id_to_srng_id_qca6390(int mac_id)
+{
+	return mac_id;
+}
+
 const struct ath11k_hw_ops ipq8074_ops = {
 	.get_hw_mac_from_pdev_id = ath11k_hw_ipq8074_mac_from_pdev_id,
 	.wmi_init_config = ath11k_init_wmi_config_qca6390,
+	.mac_id_to_pdev_id = ath11k_mac_id_to_pdev_id_qca6390,
+	.mac_id_to_srng_id = ath11k_mac_id_to_srng_id_qca6390,
 };
 
 const struct ath11k_hw_ops ipq6018_ops = {
 	.get_hw_mac_from_pdev_id = ath11k_hw_ipq6018_mac_from_pdev_id,
 	.wmi_init_config = ath11k_init_wmi_config_ipq8074,
+	.mac_id_to_pdev_id = ath11k_mac_id_to_pdev_id_ipq8074,
+	.mac_id_to_srng_id = ath11k_mac_id_to_srng_id_ipq8074,
 };
 
 const struct ath11k_hw_ops qca6390_ops = {
 	.get_hw_mac_from_pdev_id = ath11k_hw_ipq8074_mac_from_pdev_id,
+	.wmi_init_config = ath11k_init_wmi_config_qca6390,
+	.mac_id_to_pdev_id = ath11k_mac_id_to_pdev_id_qca6390,
+	.mac_id_to_srng_id = ath11k_mac_id_to_srng_id_qca6390,
 };
 
 const struct ath11k_hw_regs ipq8074_regs = {