69de124c4a03bea88d94158b18c6639ec263eb8e
Zhang Qilong <zhangqilong3@huawei.com>
2020-11-02 22:58:35 +0800

    spi: imx: fix reference leak in two imx operations
    pm_runtime_get_sync will increment pm usage counter even it
    failed. Forgetting to pm_runtime_put_noidle will result in
    reference leak in callers(spi_imx_prepare_message and
    spi_imx_remove), so we should fix it.
    
    Fixes: 525c9e5a32bd7 ("spi: imx: enable runtime pm support")
    Signed-off-by: Zhang Qilong <zhangqilong3@huawei.com>
    Link: https://lore.kernel.org/r/20201102145835.4765-1-zhangqilong3@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 1dcbdd944824369d4569959f8130336fe6fe5f39)
    Signed-off-by: Shrirang Bagul <shrirang.bagul@canonical.com>

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -1561,6 +1561,7 @@ spi_imx_prepare_message(struct spi_master *master, struct spi_message *msg)
 
 	ret = pm_runtime_get_sync(spi_imx->dev);
 	if (ret < 0) {
+		pm_runtime_put_noidle(spi_imx->dev);
 		dev_err(spi_imx->dev, "failed to enable clock\n");
 		return ret;
 	}
@@ -1805,6 +1806,7 @@ static int spi_imx_remove(struct platform_device *pdev)
 
 	ret = pm_runtime_get_sync(spi_imx->dev);
 	if (ret < 0) {
+		pm_runtime_put_noidle(spi_imx->dev);
 		dev_err(spi_imx->dev, "failed to enable clock\n");
 		return ret;
 	}
