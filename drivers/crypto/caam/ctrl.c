nxp-imx/linux-imx

MLK-18345: drivers: caam: add support for i.mx8mm
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Reviewed-by: Franck Lenormand <franck.lenormand@nxp.com>

drivers/crypto/caam/ctrl.c
@@ -599,6 +599,7 @@ static int caam_probe(struct platform_device *pdev)
 	nprop = pdev->dev.of_node;
 
 	if (!of_machine_is_compatible("fsl,imx8mq") &&
+	     !of_machine_is_compatible("fsl,imx8mm") &&
 	     !of_machine_is_compatible("fsl,imx8qm") &&
 	     !of_machine_is_compatible("fsl,imx8qxp")) {
 		ret = init_clocks(ctrlpriv);
@@ -672,6 +673,7 @@ static int caam_probe(struct platform_device *pdev)
 	}
 
 	if (!of_machine_is_compatible("fsl,imx8mq") &&
+	     !of_machine_is_compatible("fsl,imx8mm") &&
 	     !of_machine_is_compatible("fsl,imx8qm") &&
 	     !of_machine_is_compatible("fsl,imx8qxp"))
 		ctrlpriv->sm_size = resource_size(&res_regs);
@@ -735,6 +737,7 @@ iounmap_ctrl:
 	iounmap(ctrl);
 disable_clocks:
 	if (!of_machine_is_compatible("fsl,imx8mq") &&
+	     !of_machine_is_compatible("fsl,imx8mm") &&
 	     !of_machine_is_compatible("fsl,imx8qm") &&
 	     !of_machine_is_compatible("fsl,imx8qxp")) {
 		clk_disable_unprepare(ctrlpriv->caam_emi_slow);

reocat/msm-5.4

drivers/crypto/caam/ctrl.c
@@ -324,7 +324,8 @@ static int caam_probe(struct platform_device *pdev)
 
 	caam_imx = (bool)soc_device_match(imx_soc);
 
-	if (!of_machine_is_compatible("fsl,imx8mq") &&
+	if (!of_machine_is_compatible("fsl,imx8mm") &&
+		 !of_machine_is_compatible("fsl,imx8mq") &&
 	     !of_machine_is_compatible("fsl,imx8qm") &&
 	     !of_machine_is_compatible("fsl,imx8qxp")) {
 		ret = init_clocks(ctrlpriv);
@@ -397,7 +398,8 @@ static int caam_probe(struct platform_device *pdev)
 		goto disable_clocks;
 	}
 
-	if (!of_machine_is_compatible("fsl,imx8mq") &&
+	if (!of_machine_is_compatible("fsl,imx8mm") &&
+		!of_machine_is_compatible("fsl,imx8mq") &&
 	    !of_machine_is_compatible("fsl,imx8qm") &&
 	    !of_machine_is_compatible("fsl,imx8qxp")) {
 		ctrlpriv->sm_size = resource_size(&res_regs);
@@ -467,7 +469,8 @@ caam_remove:
 iounmap_ctrl:
 	iounmap(ctrl);
 disable_clocks:
-	if (!of_machine_is_compatible("fsl,imx8mq") &&
+	if (!of_machine_is_compatible("fsl,imx8mm") &&
+		!of_machine_is_compatible("fsl,imx8mq") &&
 		!of_machine_is_compatible("fsl,imx8qm") &&
 		!of_machine_is_compatible("fsl,imx8qxp")) {
 		clk_disable_unprepare(ctrlpriv->caam_emi_slow);