f8ae3fb4476307b0e65f67e9e7085da0a65a1d7d
Shrirang Bagul <shrirang.bagul@canonical.com>
2022-01-24 16:31:53 +0530

    Revert "UBUNTU: SAUCE: net: stmmac: updates to support KSZ9897 DSA cpu port"
    On BMS, the 7 port switch is connected to FEC network interface, revert
    the patch whcih configures stmicro driver as cpu port
    
    This reverts commit 23304aa2e78292ac5aec3de6c8d4eef2b265041c.
    
    Signed-off-by: Shrirang Bagul <shrirang.bagul@canonical.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac.h
@@ -23,28 +23,6 @@
 #include <linux/reset.h>
 #include <net/page_pool.h>
 
-#if defined(CONFIG_LAN937X_SWITCH)
-#ifndef CONFIG_KSZ_SWITCH
-#define CONFIG_KSZ_SWITCH
-#endif
-#endif
-
-#ifdef CONFIG_KSZ_SWITCH
-#if defined(CONFIG_HAVE_KSZ9897)
-#include "../../micrel/ksz_cfg_9897.h"
-#elif defined(CONFIG_HAVE_KSZ8795)
-#include "../../micrel/ksz_cfg_8795.h"
-#elif defined(CONFIG_HAVE_KSZ8895)
-#include "../../micrel/ksz_cfg_8895.h"
-#elif defined(CONFIG_HAVE_KSZ8863)
-#include "../../micrel/ksz_cfg_8863.h"
-#elif defined(CONFIG_HAVE_KSZ8463)
-#include "../../micrel/ksz_cfg_8463.h"
-#elif defined(CONFIG_HAVE_LAN937X)
-#include "../../microchip/lan937x_cfg.h"
-#endif
-#endif
-
 struct stmmac_resources {
 	void __iomem *addr;
 	const char *mac;
@@ -257,24 +235,6 @@ struct stmmac_priv {
 
 	/* Receive Side Scaling */
 	struct stmmac_rss rss;
-
-#ifdef CONFIG_KSZ_SWITCH
-	struct platform_device	*sw_pdev;
-	struct stmmac_priv	*hw_priv;
-	struct ksz_port		port;
-	int			phy_addr;
-	u32			multi:1;
-	u32			promisc:1;
-	u8			opened;
-	u8			hw_multi;
-	u8			hw_promisc;
-	void			*parent;
-	struct delayed_work	promisc_reset;
-	struct ksz_sw_sysfs	sysfs;
-#ifdef CONFIG_1588_PTP
-	struct ksz_ptp_sysfs	ptp_sysfs;
-#endif
-#endif
 };
 
 enum stmmac_state {
