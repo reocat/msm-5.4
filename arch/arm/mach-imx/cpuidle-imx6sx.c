986a16029a694589f900b987929876d694dea893
Isaac True <isaac.true@canonical.com>
2022-03-23 09:27:25 +0100

    UBUNTU: arm: imx: remove GPC low power mode switch
    Removing these two function calls resolves a kernel panic that occurs when
    using OP-TEE on the i.MX6SX. These calls have been removed in the upstream
    NXP kernel code in commit a70f422f264c07b825ea632afab7933e1fa4c7ba, so it
    is assumed they are not important.
    
    BugLink: https://launchpad.net/bugs/1953725 (Enable full-disk encryption (FDE) via OP-TEE (LP: #1953725))
    Signed-off-by: Isaac True <isaac.true@canonical.com>
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>

--- arch/arm/mach-imx/cpuidle-imx6sx.c
+++ arch/arm/mach-imx/cpuidle-imx6sx.c
@@ -41,7 +41,6 @@ static int imx6sx_enter_wait(struct cpuidle_device *dev,
 		break;
 	case 2:
 		imx6_enable_rbc(true);
-		imx_gpc_set_arm_power_in_lpm(true);
 		imx_set_cpu_jump(0, v7_cpu_resume);
 		/* Need to notify there is a cpu pm operation. */
 		cpu_pm_enter();
@@ -51,7 +50,6 @@ static int imx6sx_enter_wait(struct cpuidle_device *dev,
 
 		cpu_cluster_pm_exit();
 		cpu_pm_exit();
-		imx_gpc_set_arm_power_in_lpm(false);
 		imx6_enable_rbc(false);
 		break;
 	default:
