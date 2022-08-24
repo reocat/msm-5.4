boundarydevices/linux

LF-4258 remoteproc: imx_rproc: use rsc_va for resource table
When loading elf and kicking M core from Linux, previously we directly
use the address of the resource table in elf file. After i.MX8MN/P
RDC enabled to proect TCM, linux not able to access the TCM space
when updating resource table status and cause kernel dump.

Currently NXP SDK not detect the status update, and using TCM for
resource table status cause kernel dump, we switch to use
the resource table in sharded DRAM.

Reviewed-by: Ye Li <ye.li@nxp.com>
Signed-off-by: Peng Fan <peng.fan@nxp.com>

drivers/remoteproc/imx_rproc.c
@@ -772,6 +772,9 @@ imx_rproc_elf_find_loaded_rsc_table(struct rproc *rproc, const struct firmware *
 	if (priv->ipc_only)
 		return NULL;
 
+	if (priv->rsc_va)
+		return priv->rsc_va;
+
 	return rproc_elf_find_loaded_rsc_table(rproc, fw);
 }
 

reocat/msm-5.4

drivers/remoteproc/imx_rproc.c
@@ -692,6 +692,9 @@ imx_rproc_elf_find_loaded_rsc_table(struct rproc *rproc, const struct firmware *
 	if (priv->ipc_only)
 		return NULL;
 
+	if (priv->rsc_table)
+		return priv->rsc_table;
+
 	return rproc_elf_find_loaded_rsc_table(rproc, fw);
 }
 